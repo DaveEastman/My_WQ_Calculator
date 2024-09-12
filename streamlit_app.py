4732f0cb6a0994a48a7134abd38bf5024144fe44
DaveEastman <122057980+DaveEastman@users.noreply.github.com>
2024-09-12 09:00:18 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,4 @@ st.title("🎈 WQ's Interest Calculator")
 st.write(
     "Calculates the interest and final amount owed based on start and end dates, interest and starting amount owed."
 )
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Main Menu",
-        options=["All Interest", "Norgan", "Date Changes"],
-    )
 
